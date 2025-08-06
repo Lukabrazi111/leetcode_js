7a847d276992ccc7251b8920af56283b34631fad
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2025-08-06 22:32:30 +0200

    118 pascal's triangle

--- Array/Easy/118-pascals-triangle.js
+++ Array/Easy/118-pascals-triangle.js
@@ -4,16 +4,15 @@
  */
 const generate = function (numRows) {
     let arr = [];
-
     if (numRows >= 1) arr.push([1]);
     if (numRows >= 2) arr.push([1, 1]);
 
     for (let i = 2; i < numRows; i++) {
-        let prev = arr[arr.length - 1];
+        let prev = arr[i - 1];
         let row = [1];
 
-        for(let j = 1; j < prev.length; j++) {
-            row.push(prev[j] + prev[j - 1]);
+        for (let j = 1; j < prev.length; j++) {
+            row.push(prev[j - 1] + prev[j]);
         }
 
         row.push(1);
